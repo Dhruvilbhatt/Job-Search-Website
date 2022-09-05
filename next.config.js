07a6a2e2321606f9040c7a14b99e17729c3807ba
Dhruvilbhatt <dhruvilbhattlm10@gmail.com>
2022-09-05 15:04:57 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["rb.gy", "content.linkedin.com", "media.npr.org", "www.iconsdb.com", "images.fastcompany.net", "influence.digital", "encrypted-tbn0.gstatic.com"],
+  },
+};
